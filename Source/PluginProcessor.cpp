962d09b7df538378263c932367556415998beb6c
Shibanov Vasilii <pidolin88@gmail.com>
2024-07-26 17:53:25 +0300

    Refactored Peak coefficient generation
    Also drew respone curve

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -224,13 +224,19 @@ ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
     return settings;
 }
 
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate)
+{
+	return juce::dsp::IIR::Coefficients<float>::makePeakFilter(
+		   sampleRate,
+		   chainSettings.peakFreq,
+		   chainSettings.peakQuality,
+		   juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+}
+
+
 void EqualizerAudioProcessor::updatePeakFilter(const ChainSettings& chainSettings)
 {
-	auto peakCoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(
-		getSampleRate(),
-		chainSettings.peakFreq,
-		chainSettings.peakQuality,
-		juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+    auto peakCoefficients = makePeakFilter(chainSettings, getSampleRate());
 
 	updateCoefficients(leftChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
 	updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
