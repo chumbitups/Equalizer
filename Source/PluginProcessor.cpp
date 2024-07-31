8b5fe5ce5b0ef73f6dc4e3c580cd52941dbbc165
Shibanov Vasilii <pidolin88@gmail.com>
2024-07-31 18:12:25 +0300

    Added ability to bypass DSP bands

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -239,6 +239,10 @@ ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
     settings.lowCutSlope = static_cast<Slope>(apvts.getRawParameterValue("LowCut Slope")->load());
     settings.highCutSlope = static_cast<Slope>(apvts.getRawParameterValue("HighCut Slope")->load());
 
+    settings.lowCutBypassed = apvts.getRawParameterValue("LowCut Bypassed")->load() > 0.5f;
+    settings.highCutBypassed = apvts.getRawParameterValue("HighCut Bypassed")->load() > 0.5f;
+    settings.peakBypassed = apvts.getRawParameterValue("Peak Bypassed")->load() > 0.5f;
+
     return settings;
 }
 
@@ -256,6 +260,9 @@ void EqualizerAudioProcessor::updatePeakFilter(const ChainSettings& chainSetting
 {
     auto peakCoefficients = makePeakFilter(chainSettings, getSampleRate());
 
+	leftChain.setBypassed<ChainPositions::Peak>(chainSettings.peakBypassed);
+	rightChain.setBypassed<ChainPositions::Peak>(chainSettings.peakBypassed);
+
 	updateCoefficients(leftChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
 	updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
 }
@@ -268,9 +275,11 @@ void updateCoefficients(Coefficients& old, const Coefficients& replacements)
 void EqualizerAudioProcessor::updateLowCutFilters(const ChainSettings& chainSettings)
 {
     auto cutCoefficients = makeLowCutFilter(chainSettings, getSampleRate());
-
     auto& leftLowCut = leftChain.get<ChainPositions::LowCut>();
     auto& rightLowCut = rightChain.get <ChainPositions::LowCut>();
+
+    leftChain.setBypassed<ChainPositions::LowCut>(chainSettings.lowCutBypassed);
+    rightChain.setBypassed<ChainPositions::LowCut>(chainSettings.lowCutBypassed);
 
     updateCutFilter(leftLowCut, cutCoefficients, chainSettings.lowCutSlope);
     updateCutFilter(rightLowCut, cutCoefficients, chainSettings.lowCutSlope);
@@ -279,9 +288,11 @@ void EqualizerAudioProcessor::updateLowCutFilters(const ChainSettings& chainSett
 void EqualizerAudioProcessor::updateHighCutFilters(const ChainSettings& chainSettings)
 {
     auto highCutCoefficients = makeHighCutFilter(chainSettings, getSampleRate());
-
 	auto& leftHightCut = leftChain.get<ChainPositions::HighCut>();
 	auto& rightHightCut = rightChain.get<ChainPositions::HighCut>();
+
+	leftChain.setBypassed<ChainPositions::HighCut>(chainSettings.highCutBypassed);
+	rightChain.setBypassed<ChainPositions::HighCut>(chainSettings.highCutBypassed);
 
 	updateCutFilter(leftHightCut, highCutCoefficients, chainSettings.highCutSlope);
 	updateCutFilter(rightHightCut, highCutCoefficients, chainSettings.highCutSlope);
@@ -327,6 +338,12 @@ juce::AudioProcessorValueTreeState::ParameterLayout
 
 	layout.add(std::make_unique<juce::AudioParameterChoice>("LowCut Slope", "LowCut Slope", stringArray, 0));
     layout.add(std::make_unique<juce::AudioParameterChoice>("HighCut Slope", "HighCut Slope", stringArray, 0));
+
+    // Bypass buttons
+    layout.add(std::make_unique<juce::AudioParameterBool>("LowCut Bypassed", "LowCut Bypassed", false));
+    layout.add(std::make_unique<juce::AudioParameterBool>("Peak Bypassed", "Peak Bypassed", false));
+    layout.add(std::make_unique<juce::AudioParameterBool>("HighCut Bypassed", "HighCut Bypassed", false));
+    layout.add(std::make_unique<juce::AudioParameterBool>("Analyzer Enabled", "Analyzer Enabled", true));
 
     return layout;
 }
